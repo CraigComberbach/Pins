9420a8ca84fe5cb594a9e93e1c8ff4a04d7519a1
CraigComberbach <craigc@price-hvac.com>
2014-02-05 13:21:16 -0600

    v1.0.0
    Tested, works, official release.

--- Pins.c
+++ Pins.c
@@ -6,6 +6,9 @@ Code assumptions:
 Purpose:				Read the Thermistor inputs
 
 Version History:
+v1.0.0	2014-02-05	Craig Comberbach	Compiler: C30 v3.31	Optimization: 0	IDE: MPLABx 1.95	Tool: IDC3	Computer: Intel Core i5-2540 CPU 2.6 GHz, 3.95 GB RAM, Windows 7 64 bit Professional SP1
+	Added Pin Low, High, and Toggle
+	Tested, this is the official release
 v0.1.0	2014-02-01	Craig Comberbach	Compiler: C30 v3.31	Optimization: 0	IDE: MPLABx 1.95	Tool: IDC3	Computer: Intel Core i5-2540 CPU 2.6 GHz, 3.95 GB RAM, Windows 7 64 bit Professional SP1
 	Rewrote everything from scratch, removing the chip specific case staements and replacing with generic functions that should work on any chip
 v0.0.1	2013-07-21  Craig Comberbach	Compiler: XC16 v1.11	IDE: MPLABx 1.70	Tool: ICD3	Computer: Intel Core2 Quad CPU 2.40 GHz, 5 GB RAM, Windows 7 64 bit Home Premium SP1
@@ -22,9 +25,9 @@ v0.0.0	2013-07-20  Craig Comberbach	Compiler: XC16 v1.11	IDE: MPLABx 1.70	Tool: 
 #include "Pins.h"
 
 /************* Semantic Versioning***************/
-#if PINS_MAJOR != 0
+#if PINS_MAJOR != 1
 	#error "Pins.c has had a change that loses some previously supported functionality"
-#elif PINS_MINOR != 1
+#elif PINS_MINOR != 0
 	#error "Pins.c has new features that this code may benefit from"
 #elif PINS_PATCH != 0
 	#error "Pins.c has had a bug fix, you should check to see that we weren't relying on a bug for functionality"
